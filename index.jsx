ca87fddb093a3c587fcfdf8189966849271317c1
Oluwasetemi Ojo <setemiojo@gmail.com>
2023-08-19 14:21:22 -0500

    update with prop

--- index.jsx
+++ index.jsx
@@ -1,18 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Fragment } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const hello = React.createElement('p', {id: 'hello' }, 'Hello World in react');
-
-const container = React.createElement('div', {id: 'container' }, hello)
-
-const hello2 = <p id="hello">Hello World in react</p>;
-
-
-let p = document.createElement('p');
-p.textContent = 'Hello World';
 let app = document.getElementById('root');
 
-app.append(p);
+// const Hello = () => <p>Hello World</p>;
 
+// mk('p', null, 'Hello World')
+React.createElement('p', null, 'Hello World');
 
-ReactDOM.render(container, app);
+let odlObj = { className: 'class', id: 'hello', children: 'Hello World' };
+let newObj = { ...odlObj, children: '6' };
+
+// function FirstComponent(props) {
+//   console.log(props);
+//   return <p {...props}>{props.children}</p>;
+// }
+
+// desctructuring
+function FirstComponent({ children, ...rest }) {
+  // console.log(props);
+  // const {children, ...rest} = props;
+  return <p {...rest}>{children}</p>;
+}
+
+{
+  /* <FirstComponent className="class" id="hello" children='Hello World' /> */
+}
+// FirstComponent({ className: 'class', id: 'hello', children: 'Hello World' })
+// <FirstComponent className="class" id="hello" children='Hello World'></FirstComponent>
+
+function App() {
+  return (
+    <Fragment>
+      <FirstComponent
+        id="hello1"
+        className="class"
+        style={{ color: 'red' }}
+        children="Hello World"
+      ></FirstComponent>
+      <FirstComponent
+        id="hello2"
+        className="class"
+        style={{ color: 'red' }}
+        children="Hello World"
+      ></FirstComponent>
+      <FirstComponent
+        id="hello3"
+        className="class"
+        style={{ color: 'red' }}
+        children="Hello World"
+      ></FirstComponent>
+    </Fragment>
+  );
+}
+
+// ReactDOM.render(<App />, app)
+
+const root = createRoot(app);
+
+root.render(<App />);
